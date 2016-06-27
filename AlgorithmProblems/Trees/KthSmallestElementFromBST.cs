bf05a1d8d7eabd96cfa3bfc20cdf4f471fcdcd27
Jacob Kurien <jacobofficial@hotmail.com>
2016-06-26 22:11:43 -0700

    kth smallest element in bst - iterative method

--- AlgorithmProblems/Trees/KthSmallestElementFromBST.cs
+++ AlgorithmProblems/Trees/KthSmallestElementFromBST.cs
@@ -7,9 +7,24 @@ using System.Threading.Tasks;
 
 namespace AlgorithmProblems.Trees
 {
+    /// <summary>
+    /// Get the kth smallest element in a BST
+    /// </summary>
     public class KthSmallestElementFromBST
     {
         public BinarySearchTreeNode<int> KthSmallestElement { get; set; }
+
+        /// <summary>
+        /// We will traverse the BST in inorder traversal and keep track of the currentOrder
+        /// which is the order statistic for that node in the ascending order.
+        /// 
+        /// The running time is O(n)
+        /// and the space requrement is O(logn) since we are employing recursion here.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="k"></param>
+        /// <param name="currentOrder"></param>
+        /// <returns></returns>
         public int GetKthSmallestElement(BinarySearchTreeNode<int> node, int k, int currentOrder)
         {
             if(k == 0)
@@ -37,6 +52,46 @@ namespace AlgorithmProblems.Trees
 
         }
 
+        /// <summary>
+        /// Algo2: here we will do the inorder traversal in an iterative fashion.
+        /// When we do pop from the stack we can keep a count of the order and when order == k
+        /// we can return that element
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public BinarySearchTreeNode<int> GetKthSmallestElementIterative(BinarySearchTreeNode<int> node, int k)
+        {
+            Stack<BinarySearchTreeNode<int>> st = new Stack<BinarySearchTreeNode<int>>();
+            st.Push(node);
+            int currentOrder = 0;
+            bool goLeft = true;
+
+            while (st.Count > 0)
+            {
+                BinarySearchTreeNode<int> currentNode = st.Peek();
+                while (currentNode.Left != null && goLeft)
+                {
+                    st.Push(currentNode.Left);
+                    currentNode = currentNode.Left;
+                }
+                goLeft = false;
+                currentNode = st.Pop();
+                if(++currentOrder == k)
+                {
+                    // We have found the kth smallest element
+                    return currentNode;
+                }
+                if(currentNode.Right!=null)
+                {
+                    st.Push(currentNode.Right);
+                    goLeft = true;
+                }
+
+            }
+            return null;
+        }
+
         public static void TestKthSmallestElementFromBST()
         {
             BinarySearchTree<int> bst = new BinarySearchTree<int>();
@@ -54,22 +109,32 @@ namespace AlgorithmProblems.Trees
             KthSmallestElementFromBST kObj = new KthSmallestElementFromBST();
             kObj.GetKthSmallestElement(bst.Head, 5, 0);
             Console.WriteLine("The {0}th element in the bst is {1}", 5, kObj.KthSmallestElement.Data);
-
+            BinarySearchTreeNode<int> kthSmallestElement = kObj.GetKthSmallestElementIterative(bst.Head, 5);
+            Console.WriteLine("Algo2: The {0}th element in the bst is {1}", 5, kthSmallestElement.Data);
+            
             kObj = new KthSmallestElementFromBST();
             kObj.GetKthSmallestElement(bst.Head, 10, 0);
             Console.WriteLine("The {0}th element in the bst is {1}", 10, (kObj.KthSmallestElement==null) ? -1 : kObj.KthSmallestElement.Data);
+            kthSmallestElement = kObj.GetKthSmallestElementIterative(bst.Head, 10);
+            Console.WriteLine("Algo2: The {0}th element in the bst is {1}", 10, (kthSmallestElement == null) ? -1 : kthSmallestElement.Data);
 
             kObj = new KthSmallestElementFromBST();
             kObj.GetKthSmallestElement(bst.Head, 0, 0);
             Console.WriteLine("The {0}th element in the bst is {1}", 0, (kObj.KthSmallestElement == null) ? -1 : kObj.KthSmallestElement.Data);
+            kthSmallestElement = kObj.GetKthSmallestElementIterative(bst.Head, 0);
+            Console.WriteLine("Algo2: The {0}th element in the bst is {1}", 0, (kthSmallestElement == null) ? -1 : kthSmallestElement.Data);
 
             kObj = new KthSmallestElementFromBST();
             kObj.GetKthSmallestElement(bst.Head, 7, 0);
             Console.WriteLine("The {0}th element in the bst is {1}", 7, (kObj.KthSmallestElement == null) ? -1 : kObj.KthSmallestElement.Data);
+            kthSmallestElement = kObj.GetKthSmallestElementIterative(bst.Head, 7);
+            Console.WriteLine("Algo2: The {0}th element in the bst is {1}", 7, (kthSmallestElement == null) ? -1 : kthSmallestElement.Data);
 
             kObj = new KthSmallestElementFromBST();
             kObj.GetKthSmallestElement(bst.Head, 1, 0);
             Console.WriteLine("The {0}th element in the bst is {1}", 1, (kObj.KthSmallestElement == null) ? -1 : kObj.KthSmallestElement.Data);
+            kthSmallestElement = kObj.GetKthSmallestElementIterative(bst.Head, 1);
+            Console.WriteLine("Algo2: The {0}th element in the bst is {1}", 1, (kthSmallestElement == null) ? -1 : kthSmallestElement.Data);
         }
     }
 }
