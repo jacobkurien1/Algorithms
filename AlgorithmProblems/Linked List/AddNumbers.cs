3ec503bf02b7890eb5118418729d14034b8a0a8a
Jacob Kurien <jacobofficial@hotmail.com>
2016-08-09 11:26:31 -0700

    Add numbers in linked list - edit

--- AlgorithmProblems/Linked List/AddNumbers.cs
+++ AlgorithmProblems/Linked List/AddNumbers.cs
@@ -15,7 +15,7 @@ namespace AlgorithmProblems.Linked_List
             SingleLinkedListNode<int> head = null;
             SingleLinkedListNode<int> currentSum = null;
 
-            while( num1 != null && num2 != null)
+            while( num1 != null || num2 != null)
             {
                 num1Val = num1 != null ? num1.Data : 0;
                 num2Val = num2 != null ? num2.Data : 0;
@@ -41,8 +41,14 @@ namespace AlgorithmProblems.Linked_List
                     currentSum.NextNode = new SingleLinkedListNode<int>(sum);
                     currentSum  = currentSum.NextNode;
                 }
-                num1 = num1.NextNode;
-                num2 = num2.NextNode;
+                if (num1 != null)
+                {
+                    num1 = num1.NextNode;
+                }
+                if (num2 != null)
+                {
+                    num2 = num2.NextNode;
+                }
             }
 
             if(carry==1)
@@ -73,18 +79,93 @@ namespace AlgorithmProblems.Linked_List
                 carry = 0;
             }
             SingleLinkedListNode<int> sumNode = new SingleLinkedListNode<int>(sum);
-            sumNode.NextNode = AddNumbersRepresentedByLinkedListAlgo2(num1.NextNode, num2.NextNode, carry);
+            sumNode.NextNode = AddNumbersRepresentedByLinkedListAlgo2((num1!=null)?num1.NextNode:null, (num2!=null)?num2.NextNode:null, carry);
             return sumNode;
         }
+
+        #region Algo3: naive approach
+        /// <summary>
+        /// Given 2 linked list, we should do a sum of them and return the result in a linkedlist
+        /// ll1 = 1->5->3
+        /// ll2 = 4->7->2
+        /// 351+274 = 625
+        /// so return 5->2->6
+        /// </summary>
+        /// <param name="ll1"></param>
+        /// <param name="ll2"></param>
+        /// <returns></returns>
+        public SingleLinkedListNode<int> AddLinkedLists(SingleLinkedListNode<int> ll1, SingleLinkedListNode<int> ll2)
+        {
+            int num1 = GetNumFromLL(ll1);
+            int num2 = GetNumFromLL(ll2);
+            int sum = num1 + num2;
+            return GetLLFromNum(sum);
+        }
+        /// <summary>
+        /// Gets the number from the linked list
+        /// For eg
+        /// 2->5->1 must yield a num 152
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        private int GetNumFromLL(SingleLinkedListNode<int> node)
+        {
+            int finalNum = 0;
+            int multiplicationFactor = 1;
+            while (node != null)
+            {
+                finalNum += (node.Data * multiplicationFactor);
+                node = node.NextNode;
+                multiplicationFactor *= 10;
+            }
+            return finalNum;
+        }
+
+        /// <summary>
+        /// This will convert a number to a linked list
+        /// for eg:
+        /// num 152 will be converted to 2->5->1
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private SingleLinkedListNode<int> GetLLFromNum(int num)
+        {
+            if (num == 0)
+            {
+                return new SingleLinkedListNode<int>(0);
+            }
+            SingleLinkedListNode<int> linkedListHead = null;
+            SingleLinkedListNode<int> linkedListCurrent = null;
+
+            while (num > 0)
+            {
+                int currentLLData = num % 10;
+                if (linkedListCurrent == null)
+                {
+                    // Case where we need to set the linkedlist head 
+                    linkedListCurrent = new SingleLinkedListNode<int>(currentLLData);
+                    linkedListHead = linkedListCurrent;
+                }
+                else
+                {
+                    // case where we have the linkedlist head and we need to add the new node at the current's next node
+                    linkedListCurrent.NextNode = new SingleLinkedListNode<int>(currentLLData);
+                    linkedListCurrent = linkedListCurrent.NextNode;
+                }
+                num /= 10;
+            }
+            return linkedListHead;
+        }
+        #endregion
 
         public static void TestAddNumbersRepresentedByLinkedList()
         {
             AddNumbers adNums = new AddNumbers();
 
-            SingleLinkedListNode<int> ll1 = LinkedListHelper.CreateSinglyLinkedList(10);
+            SingleLinkedListNode<int> ll1 = LinkedListHelper.CreateSinglyLinkedList(6);
             LinkedListHelper.PrintSinglyLinkedList(ll1);
 
-            SingleLinkedListNode<int> ll2 = LinkedListHelper.CreateSinglyLinkedList(10);
+            SingleLinkedListNode<int> ll2 = LinkedListHelper.CreateSinglyLinkedList(7);
             LinkedListHelper.PrintSinglyLinkedList(ll2);
 
             Console.WriteLine("The sum with algo 1 is as follows:");
@@ -93,6 +174,10 @@ namespace AlgorithmProblems.Linked_List
 
             Console.WriteLine("The sum with algo 2 is as follows:");
             sum = adNums.AddNumbersRepresentedByLinkedListAlgo2(ll1, ll2, 0);
+            LinkedListHelper.PrintSinglyLinkedList(sum);
+
+            Console.WriteLine("The sum with algo 3 is as follows:");
+            sum = adNums.AddLinkedLists(ll1, ll2);
             LinkedListHelper.PrintSinglyLinkedList(sum);
 
         }
