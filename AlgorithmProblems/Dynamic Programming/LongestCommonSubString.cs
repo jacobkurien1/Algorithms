c7eb8f366c462ba0ca776f1b68bb1c403bc700f7
Jacob Kurien <jacobofficial@hotmail.com>
2016-09-02 14:10:22 -0700

    longest common substring bug fixed

--- AlgorithmProblems/Dynamic Programming/LongestCommonSubString.cs
+++ AlgorithmProblems/Dynamic Programming/LongestCommonSubString.cs
@@ -12,14 +12,8 @@ namespace AlgorithmProblems.Dynamic_Programming
         /// Get the longest common substring between 2 input strings str1 and str2
         /// 
         /// We will solve this problem using dynamic programming
-        /// LCS(JACOB, JACOX) = max{LCS(JACOB, JACO), LCS(JACO, JACOX)} , since the last char in both the strings are different
+        /// LCS(JACOB, JACOX) = 0 , since the last char in both the strings are different
         /// LCS(XJACOB, YJACOB) = 1+ LCS(XJACO, YJACO) , since the last char in both the strings are same
-        /// 
-        /// We will also use a pointer table to help us back track and find the longest substring
-        /// The different values in the pointer table are:
-        /// 0-> match
-        /// 1-> horizontal(go left while backtracking)
-        /// 2-> vertical(go up while backtracking)
         /// 
         /// </summary>
         /// <param name="str1">input string</param>
@@ -28,17 +22,8 @@ namespace AlgorithmProblems.Dynamic_Programming
         private static string GetLongestCommonSubString(string str1, string str2)
         {
             int[,] lcsVal = new int[str1.Length, str2.Length];
-            int[,] pointerTable = new int[str1.Length, str2.Length];
-
-            //initialization
-            for(int i=0; i<str1.Length; i++)
-            {
-                pointerTable[i, 0] = 1;
-            }
-            for (int j = 0; j < str1.Length; j++)
-            {
-                pointerTable[0, j] = 2;
-            }
+            int maxLCSVal = 0;
+            int maxLCSValIndex = -1;
 
             for(int i=0; i<str1.Length; i++)
             {
@@ -46,56 +31,28 @@ namespace AlgorithmProblems.Dynamic_Programming
                 {
                     if(str1[i] == str2[j])
                     {
-                        lcsVal[i, j] = 1 + ((i==0||j==0)? 0: lcsVal[i - 1, j - 1]);
-                        pointerTable[i, j] = 0; // we have a one char match here
-                    }
-                    else
-                    {
-                        if(j>0 && lcsVal[i, j] < lcsVal[i, j-1])
+                        lcsVal[i, j] = 1 + (( i-1<0 || j-1 <0 )? 0: lcsVal[i - 1, j - 1]);
+                        if(maxLCSVal < lcsVal[i, j])
                         {
-                            lcsVal[i, j] = lcsVal[i, j - 1];
-                            pointerTable[i, j] = 1; // go left while backtracking
-                        }
-                        if (i>0 && lcsVal[i, j] < lcsVal[i-1, j])
-                        {
-                            lcsVal[i, j] = lcsVal[i - 1, j];
-                            pointerTable[i, j] = 2; // go up while backtracking
+                            maxLCSVal = lcsVal[i, j];
+                            maxLCSValIndex = i;
                         }
                     }
                 }
             }
 
-            int iIndex = str1.Length-1;
-            int jIndex = str2.Length-1;
-            Console.WriteLine("The longest common substring is having a length of {0}", lcsVal[iIndex, jIndex]);
-            char[] lcsString = new char[lcsVal[iIndex, jIndex]];
-            int lcsStringIndex = lcsVal[iIndex, jIndex]-1;
-            while(iIndex>=0 && jIndex>=0)
-            {
-                if(pointerTable[iIndex, jIndex] == 0)
-                {
-                    // We have a match here
-                    lcsString[lcsStringIndex--] = str1[iIndex];
-                    iIndex--;
-                    jIndex--;
-                }
-                else if(pointerTable[iIndex, jIndex] == 1)
-                {
-                    jIndex--;
-                }
-                else // if (pointerTable[iIndex, jIndex] == 2)
-                {
-                    iIndex--;
-                }
-            }
-            return new string(lcsString);
+            return str1.Substring(maxLCSValIndex - maxLCSVal + 1, maxLCSVal);
         }
 
         public static void TestGetLongestCommonSubString()
         {
             Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "XJacobY", "YJacobX", GetLongestCommonSubString("XJacobY", "YJacobX"));
             Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "XJacob", "YJacob", GetLongestCommonSubString("XJacob", "YJacob"));
-            Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "JacobY", "JacobX", GetLongestCommonSubString("JacobY", "JacobX"));
+            Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "JacobYL", "JacobXL", GetLongestCommonSubString("JacobYL", "JacobXL"));
+            Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "bbbbJacob", "Jacob", GetLongestCommonSubString("bbbbJacob", "Jacob"));
+            Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "Jacobbbbbb", "Jacob", GetLongestCommonSubString("Jacobbbbbb", "Jacob"));
+            Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "Jacob", "bbbbJacob", GetLongestCommonSubString("Jacob", "bbbbJacob"));
+            Console.WriteLine("The longest common substring of strings {0} and {1} is {2}", "Jacob", "Jacobbbbbb", GetLongestCommonSubString("Jacob", "Jacobbbbbb"));
         }
     }
 }
