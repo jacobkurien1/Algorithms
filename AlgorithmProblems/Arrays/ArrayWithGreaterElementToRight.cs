f78d2d7a0857ff5a031afc51aeff83674edb3da7
Jacob Kurien <jacobofficial@hotmail.com>
2018-03-19 09:28:54 -0700

    cleaner approach for array with greater element to right

--- AlgorithmProblems/Arrays/ArrayWithGreaterElementToRight.cs
+++ AlgorithmProblems/Arrays/ArrayWithGreaterElementToRight.cs
@@ -14,6 +14,8 @@ namespace AlgorithmProblems.Arrays
     /// </summary>
     class ArrayWithGreaterElementToRight
     {
+
+        #region Algo1 
         /// <summary>
         /// The naive approach is to fo from left to right and for each index go from index to the end of the array 
         /// and get the next greater element than the current element. The running time is O(n^2).
@@ -53,6 +55,48 @@ namespace AlgorithmProblems.Arrays
             return retArr;
         }
 
+        #endregion
+
+        #region Algo2 - Cleaner approach
+        /// <summary>
+        /// Algo: 1. Whenever arr[i] < arr[i+1] then we have indexArr[i] = i+1
+        /// 2. Else we can keep poping from stack till stack.top < arr[i] and make index[stack.top] = i
+        /// Also push i into stack.
+        /// 
+        /// Running time O(n)
+        /// Space requriement O(n)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int[] GetIndexArray2(int[] arr)
+        {
+            if (arr == null)
+            {
+                throw new ArgumentException("The input array is not valid");
+            }
+
+            int[] retArr = Enumerable.Repeat(-1, arr.Length).ToArray(); // Initialize all array elements to -1
+            Stack<int> st = new Stack<int>();
+            for (int index = 0; index < arr.Length; index++)
+            {
+                if((index + 1<arr.Length) && arr[index] < arr[index + 1])
+                {
+                    retArr[index] = index + 1;
+                }
+                else
+                {
+                    while(st.Count >0 && arr[st.Peek()] < arr[index])
+                    {
+                        retArr[st.Pop()] = index;
+                    }
+                    st.Push(index);
+                }
+            }
+            return retArr;
+        }
+
+        #endregion
+
         public static void TestArrayWithGreaterElementToRight()
         {
             int[] arr = new int[] { 5, 1, 2, 1, 3, 2, 9 };
@@ -61,6 +105,8 @@ namespace AlgorithmProblems.Arrays
             ArrayHelper.PrintArray(retArrIndex);
             Console.WriteLine("The actual index array is:");
             ArrayHelper.PrintArray(GetIndexArray(arr));
+            ArrayHelper.PrintArray(GetIndexArray2(arr));
+
 
             arr = new int[] { 23, 45, 2, 1, 3, 2, 9 };
             retArrIndex = new int[] { 1, -1, 4, 4, 6, 6, -1 };
@@ -68,6 +114,7 @@ namespace AlgorithmProblems.Arrays
             ArrayHelper.PrintArray(retArrIndex);
             Console.WriteLine("The actual index array is:");
             ArrayHelper.PrintArray(GetIndexArray(arr));
+            ArrayHelper.PrintArray(GetIndexArray2(arr));
 
         }
     }
