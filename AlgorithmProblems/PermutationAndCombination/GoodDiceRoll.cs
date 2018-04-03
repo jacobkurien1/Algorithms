c765c0b190d562f6aeee2c04adb4cc67463e7175
Jacob Kurien <jacobofficial@hotmail.com>
2018-04-03 04:25:00 -0700

    good dice roll iterative

--- AlgorithmProblems/PermutationAndCombination/GoodDiceRoll.cs
+++ AlgorithmProblems/PermutationAndCombination/GoodDiceRoll.cs
@@ -11,6 +11,8 @@ namespace AlgorithmProblems.PermutationAndCombination
     /// </summary>
     public class GoodDiceRoll
     {
+
+        #region Recursive solution
         /// <summary>
         /// Algo: 1. Get all the combinations of n dice rolls.
         /// 2. Filter the list by the combinations which satisfy sum%mod == product%mod
@@ -79,6 +81,79 @@ namespace AlgorithmProblems.PermutationAndCombination
             return prod;
         }
 
+        #endregion
+
+        #region Iterative solution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mod">mod value so that we dont have overflow for sum and product</param>
+        /// <param name="maxDiceRolls">max number of dice rolls that needs to be performed</param>
+        /// <returns></returns>
+        public static int GetCountOfGoodDiceRolls(int mod, int maxDiceRolls)
+        {
+            //1. initialization
+            List<DiceRolls> previous = new List<DiceRolls>();
+            List<DiceRolls> current = new List<DiceRolls>();
+            previous.Add(new DiceRolls(mod));
+
+            //2. get all the dice rolls
+            for(int i =  1; i<=maxDiceRolls; i++)
+            {
+                foreach(DiceRolls dr in previous)
+                {
+                    for (int diceVal = 1; diceVal <= 6; diceVal++)
+                    {
+                        DiceRolls drClone = dr.Clone();
+                        drClone.Add(diceVal);
+                        current.Add(drClone);
+                    }
+                }
+                previous = current;
+                current = new List<DiceRolls>();
+            }
+
+            //3. Get all the good rolls
+            return previous.Where(x => x.ProdMod == x.SumMod).Count();
+        }
+
+        /// <summary>
+        /// Represents a list of dice rolls with mod of sum of all elements
+        /// and mod of product of all elements
+        /// </summary>
+        class DiceRolls
+        {
+            private List<int> AllDiceRolls;
+            private int Mod;
+            public int SumMod { get; set; }
+            public int ProdMod { get; set; }
+
+            public DiceRolls(int mod)
+            {
+                AllDiceRolls = new List<int>();
+                Mod = mod;
+                ProdMod = 1;
+            }
+
+            public void Add(int diceVal)
+            {
+                AllDiceRolls.Add(diceVal);
+                SumMod = ((diceVal % Mod) + SumMod) % Mod;
+                ProdMod = ((diceVal % Mod) * ProdMod) % Mod;
+            }
+
+            public DiceRolls Clone()
+            {
+                DiceRolls clone = new DiceRolls(Mod);
+                clone.AllDiceRolls = new List<int>(AllDiceRolls);
+                clone.SumMod = SumMod;
+                clone.ProdMod = ProdMod;
+                return clone;
+            }
+        }
+
+        #endregion
+
         /// <summary>
         /// Test method
         /// </summary>
@@ -88,6 +163,11 @@ namespace AlgorithmProblems.PermutationAndCombination
             int mod = 5;
             Console.WriteLine("The good dice for {2} rolls with mod value as {3} are {0}. Expected: {1}",
                 GetGoodDiceRoll(2, 5),
+                4,
+                n,
+                mod);
+            Console.WriteLine("The good dice for {2} rolls with mod value as {3} are {0}. Expected: {1}",
+                GetCountOfGoodDiceRolls(mod, n),
                 4,
                 n,
                 mod);
